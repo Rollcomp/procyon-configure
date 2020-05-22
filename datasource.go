5ded6133854706f127f6118cab8736ff78b27f04
Burak Koken <burakkokenn@gmail.com>
2020-05-22 02:54:05 +0200

    Created Data Source and Server Properties.

---
+++ datasource.go
@@ -0,0 +1,34 @@
+package configure
+
+import (
+	context "github.com/Rollcomp/procyon-context"
+	"github.com/codnect/go-one"
+)
+
+type DataSourceProperties struct {
+	DriverName   string `yaml:"driver-name" json:"driver-name"`
+	Username     string `yaml:"username" json:"username"`
+	Password     string `yaml:"password" json:"password"`
+	DatabaseName string `yaml:"database-name" json:"database-name"`
+}
+
+func (properties *DataSourceProperties) GetPrefix() string {
+	return "procyon.datasource"
+}
+
+type DataSourceConfiguration struct {
+}
+
+func NewDataSourceAutoConfiguration() DataSourceConfiguration {
+	return DataSourceConfiguration{}
+}
+
+func (config DataSourceConfiguration) Register() []one.Func {
+	return nil
+}
+
+func (config DataSourceConfiguration) ConfigurationProperties() []context.ConfigurationProperties {
+	return []context.ConfigurationProperties{
+		&DataSourceProperties{},
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Rollcomp/procyon-configure
 
 go 1.13
+
+require (
+	github.com/Rollcomp/procyon-context v0.0.0-20200522004933-00018a90c862
+	github.com/codnect/go-one v0.0.0-20200119222923-a2229e1e905b
+)
