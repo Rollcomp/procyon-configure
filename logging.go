71e0139ae0f7939d32073166287ccb213dcc5785
Burak Köken <burakkokenn@gmail.com>
2021-01-08 00:26:10 +0200

    Updated the logging properties(added file path and updated property names)

--- logging.go
+++ logging.go
@@ -1,8 +1,9 @@
 package configure
 
 type LoggingProperties struct {
-	Level    string `yaml:"level" json:"level" default:"TRACE"`
+	Level    string `yaml:"level" json:"level" default:"DEBUG"`
 	FileName string `yaml:"file.name" json:"file.name"`
+	FilePath string `yaml:"file.path" json:"file.path"`
 }
 
 func (properties *LoggingProperties) GetConfigurationPrefix() string {
